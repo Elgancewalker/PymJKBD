ff3de9fe2d5c892e922fa2d5b5b290139d531571
Elgancewalker <1697529302@qq.com>
2017-06-30 15:59:06 +0800

    设置闪屏界面

---
+++ app/src/main/java/com/example/administrator/myjkbd/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.administrator.myjkbd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/30.
+ */
+
+public class SplashActivity extends AppCompatActivity
+    {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splah);
+            mCountDownTimer.start();
+        }
+        CountDownTimer mCountDownTimer=new CountDownTimer(2000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+            @Override
+            public void onFinish() {
+                Intent intent=new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        };
+}
