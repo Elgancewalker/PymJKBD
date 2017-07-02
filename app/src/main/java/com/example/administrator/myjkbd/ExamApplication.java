02aab28730628dc9e7fe705952129af21032fdea
Elgancewalker <1697529302@qq.com>
2017-07-02 19:38:47 +0800

    从网上获取随机测试的题目

--- app/src/main/java/com/example/administrator/myjkbd/ExamApplication.java
+++ app/src/main/java/com/example/administrator/myjkbd/ExamApplication.java
@@ -5,7 +5,9 @@ import android.util.Log;
 
 import com.example.administrator.myjkbd.bean.ExamIfor;
 import com.example.administrator.myjkbd.bean.ExamQuestion;
+import com.example.administrator.myjkbd.bean.Result;
 import com.example.administrator.myjkbd.utils.OkHttpUtils;
+import com.example.administrator.myjkbd.utils.ResultUtils;
 
 import java.util.List;
 
@@ -28,27 +30,63 @@ public class ExamApplication extends Application {
         return instance;
     }
     private void initData() {
-        OkHttpUtils<ExamIfor> utils=new OkHttpUtils<>(getApplicationContext());
-        String uri="http://101.251.196.90:8080/JztkServer/examInfo";
-        utils.url(uri)
-                .targetClass(ExamIfor.class)
-                .execute(new OkHttpUtils.OnCompleteListener<ExamIfor>(){
-                    @Override
-                    public void onSuccess(ExamIfor result) {
-                        Log.e("main","result="+result);
-                        mExamIfor=result;
-                    }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpUtils<ExamIfor> utils=new OkHttpUtils<>(getApplicationContext());
+                String url ="http://101.251.196.90:8080/JztkServer/examInfo";
+                utils.url(url)
+                        .targetClass(ExamIfor.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<ExamIfor>(){
+                            @Override
+                            public void onSuccess(ExamIfor result) {
+                                Log.e("main","result="+result);
+                                mExamIfor=result;
+                            }
 
-                    @Override
-                    public void onError(String error) {
-                        Log.e("main","error="+error);
-                    }
-                });
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error="+error);
+                            }
+                        });
+
+                OkHttpUtils<String>utils1=new OkHttpUtils<String>(instance);
+                String url2="http://101.251.196.90:8080/JztkServer/getQuestions?testType=rand";
+                utils1.url(url2)
+                        .targetClass(String.class)
+                        .execute(new OkHttpUtils.OnCompleteListener<String>(){
+
+                            @Override
+                            public void onSuccess(String jsonStr) {
+                                Result result= ResultUtils.getListResultFromJson(jsonStr);
+                                if(result!=null&&result.getError_code()==0){
+                                    List<ExamQuestion> list=result.getResult();
+                                    if(list!=null&&list.size()>0){
+                                        mExamList=list;
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onError(String error) {
+                                Log.e("main","error="+error);
+                            }
+                        });
+
+                }
+        }).start();
     }
     public ExamIfor getmExamIfor(){
         return mExamIfor;
     }
     public void setmExamIfor(ExamIfor examInfo){
         mExamIfor=examInfo;
+    }
+
+    public List<ExamQuestion> getExamList() {
+        return mExamList;
+    }
+    public void setExamList(List<ExamQuestion> examList){
+        mExamList=examList;
     }
 }
--- app/src/main/java/com/example/administrator/myjkbd/activity/ExamActivity.java
+++ app/src/main/java/com/example/administrator/myjkbd/activity/ExamActivity.java
@@ -3,18 +3,24 @@ package com.example.administrator.myjkbd.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.administrator.myjkbd.ExamApplication;
 import com.example.administrator.myjkbd.R;
 import com.example.administrator.myjkbd.bean.ExamIfor;
+import com.example.administrator.myjkbd.bean.ExamQuestion;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/6/30.
  */
 
 public class ExamActivity extends AppCompatActivity {
-    TextView tvExamInfo;
+    TextView tvExamInfo,tvExamTitle,tvop1,tvop2,tvop3,tvop4;
+    ImageView mImageView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,14 +31,37 @@ public class ExamActivity extends AppCompatActivity {
 
     private void initView() {
         tvExamInfo=(TextView)findViewById(R.id.tv_examinfo);
+        tvExamTitle=(TextView)findViewById(R.id.tv_title);
+        tvop1=(TextView)findViewById(R.id.tv_op1);
+        tvop2=(TextView)findViewById(R.id.tv_op2);
+        tvop3=(TextView)findViewById(R.id.tv_op3);
+        tvop4=(TextView)findViewById(R.id.tv_op4);
+
     }
 
     private void initData() {
-        ExamIfor examIfor= ExamApplication.getInstance().getmExamIfor();
+       ExamIfor examIfor= ExamApplication.getInstance().getmExamIfor();
         if(examIfor!=null){
             showData(examIfor);
         }
+        List<ExamQuestion> examList=ExamApplication.getInstance().getExamList();
+        if(examList!=null){
+            showExam(examList);
+        }
     }
+private void showExam(List<ExamQuestion> examList) {
+    ExamQuestion exam=examList.get(0);
+    if(exam!=null)
+    {
+        tvExamTitle.setText(exam.getQuestion());
+        tvop1.setText(exam.getItem1());
+        tvop2.setText(exam.getItem2());
+        tvop3.setText(exam.getItem3());
+        tvop4.setText(exam.getItem4());
+    }
+}
+
+
     private void showData(ExamIfor examIfo) {
         tvExamInfo.setText(examIfo.toString());
     }
